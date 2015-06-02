b7dd8657f7383d0fcd65a194b8bc2778ca4e55b9
Vinzenz Aubry <vinzenz@weboffice.de>
2015-06-02 23:55:08 +0200

    tretest angelegt und mit collabtest_vinni rumgespielt

---
+++ collabstory_vinni/js/main.js
@@ -0,0 +1,19 @@
+// // smooth scroll
+// $(".scroll").click(function(event){     
+// 	event.preventDefault();
+// 	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+// });
+        
+
+// // header fade
+// $(window).scroll(function() {
+//         $(".text-vertical-center").css({
+//          'opacity' : 1-(($(this).scrollTop())/350)
+//             });    
+    
+// }); 
+
+
+$( ".buttontest" ).click(function() {
+  console.log( d3.json() );
+});
